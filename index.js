15f351bca4deb2fad28e2e3e9e09a5754618d0af
Adam Mukharil Bachtiar <Adam Mukharil Bachtiar>
2016-06-21 14:46:23 +0700

    kedua

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var express = require('express'),
   rout = require('./routes/dataRoute');
 
 // mongoose.connect('mongodb://127.0.0.1/datagraph');
-mongoose.connect('mongodb://restapi:henra12@ds011933.mlab.com:11933/dbrest');
+// mongoose.connect('mongodb://restapi:henra12@ds011933.mlab.com:11933/dbrest');
+mongoose.connect('mongodb://arifin:naonwae1@ds021034.mlab.com:21034/dbcommerce');
 var app = express();
 var port = process.env.PORT || 3000;
 var router = express.Router();
