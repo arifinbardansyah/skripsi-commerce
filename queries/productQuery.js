c1f2df38502f9ced66b8bfc40367d529b0972587
Adam Mukharil Bachtiar <Adam Mukharil Bachtiar>
2016-06-27 13:39:21 +0700

    product

---
+++ queries/productQuery.js
@@ -0,0 +1,29 @@
+var productModel = require('../models/productModel');
+
+// get all data from user data
+var getAllData = function(err, res) {
+  productModel.find(function(err, data) {
+    if (err) {
+      res.send(err);
+    }
+    // res.json(data);
+    res.json(data);
+  })
+}
+
+
+// // get one data from user data by id
+// var getProduct = function(req, res) {
+//   productModel.find({
+//     category: req.params.category
+//   }, function(err, data) {
+//     if (err) {
+//       res.send(err);
+//     }
+
+//     res.json(data);
+//   })
+// }
+
+module.exports.getAllData = getAllData;
+// module.exports.getProduct = getProduct;
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var queries = require('../queries/categoryQuery');
+var query = require('../queries/productQuery');
 
 var categoryRoute = express.Router();
 
-categoryRoute.route('/')
+categoryRoute.route('/category/')
   .get(queries.getAllData)
   .post(queries.getProduct);
+
+categoryRoute.route('/product/')
+  .get(query.getAllData);
 
 module.exports.categoryRoute = categoryRoute;
