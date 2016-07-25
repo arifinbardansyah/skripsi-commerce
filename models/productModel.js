a7d07616a695e290e58fe6f7e174ee8069c6587d
Adam Mukharil Bachtiar <Adam Mukharil Bachtiar>
2016-07-25 10:30:41 +0700

    produk

--- models/productModel.js
+++ models/productModel.js
@@ -1,9 +1,19 @@
 var mongoose = require('mongoose');
 
 var product = new mongoose.Schema({
-  id: Number,
+  _id: String,
   product: String,
-  category: String
+  category: [String],
+  image: [String],
+  brand: String,
+  rating: Number,
+  jumlah_review:Number,
+  harga: Number,
+  diskon: Number,
+  harga_diskon: Number,
+  deskripsi: String,
+  detail: String,
+  spesifikasi:String 
 });
 
 module.exports = mongoose.model('product', product, 'product');
